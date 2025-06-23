cc4e4a781d59295960089db89e5fa370360b9543
Nezareeen <nezareeen@gmail.com>
2025-06-23 14:57:58 +0530

    CA Done

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
+
+const app = express();
+
+const PORT = process.env.PORT || 7868;
+
+app.use(express.json());
+
+const users = [
+    { id: 1, name: 'Alice', age: 25 },
+    { id: 2, name: 'Bob', age: 30 },
+    { id: 3, name: 'Charlie', age: 35 }
+];
+
+app.get('/users', (req, res) => {
+    try {
+        const userId = parseInt(req.query.id);
+        if (!userId) {
+            return res.status(400).send("User ID is required");
+        }
+        
+        const user = users.find(u => u.id === userId);
+        if (!user) {
+            return res.status(404).send("User not found");
+        }
+        
+        res.status(200).send({message: "User found", user: user});
+    } catch (error) {
+        console.log("Something went wrong", error);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+app.get('/', (req,res) => {
+    try {
+        res.status(200).send("Server is running successfully!");
+    } catch (error) {
+        console.log("Something went wrong", error);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log("Successfully connected to MongoDB");
+    app.listen(PORT, () => {
+        console.log(`Server is running on port http://localhost:${PORT}`);
+    })
+});
